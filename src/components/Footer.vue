35ee2b01871c8c1f960a9133a2494972e77fa9b9
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-06-15 09:23:17 +0700

    aplikasi kulineran

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="mt-5 mb-5">
+      <hr />
+      <div class="container">
+          <div class="row">
+              <div class="col text-center">
+                  <span>&#169;</span> bukumenu.makarimresto
+              </div>
+          </div>
+      </div>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+</style>
